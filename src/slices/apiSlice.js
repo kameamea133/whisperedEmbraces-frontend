545f05c100d2f1308fa71216c1a4299f220c53d9
kameamea133 <monnier1977@gmail.com>
2024-11-05 21:48:33 +0100

    environment variable added

--- src/slices/apiSlice.js
+++ src/slices/apiSlice.js
@@ -1,10 +1,9 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const baseQuery = fetchBaseQuery({ baseUrl: "http://localhost:5000", credentials: "include" });
+const baseQuery = fetchBaseQuery({ baseUrl: import.meta.env.VITE_API_BASE_URL, credentials: "include" });
 
 export const apiSlice = createApi({
     baseQuery,
     tagTypes: ["User", "Post"],
-    // eslint-disable-next-line no-unused-vars
     endpoints: (builder) => ({}),
 });
